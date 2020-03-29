31d7780cf8d31c56fc99c7ab8aac0e9ff9d6383e
Lanhao Mao <lmao@wpi.edu>
2020-03-29 14:59:21 -0400

    collision checking node

---
+++ entertainer_pcl/src/collision_check.cpp
@@ -0,0 +1,115 @@
+#include "ros/ros.h"
+#include "std_msgs/Bool.h"
+#include <geometry_msgs/PoseArray.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <sstream>
+#include <vector>
+
+class collision_checker
+{
+  public:
+  // NodeHandle 
+  ros::NodeHandle n;
+
+  // publisher for the collision checking result
+  ros::Publisher collision_pub = n.advertise<std_msgs::Bool>("collision_check", 100);
+
+  // subscriber for point cloud input
+  ros::Subscriber point_cloud_sub = n.subscribe("/camera/depth/points", 1, &collision_checker::pointCloudCallback, this);
+
+  // subscriber for trajectory input
+  ros::Subscriber trajectory_sub = n.subscribe("/Traj_arc_path", 1, &collision_checker::trajectoryCallback, this);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr plane_cloud;
+
+  // callback function for receiving the point cloud  
+  void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& input_cloud)
+  {
+    // the current input point cloud 
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // conversion to PCL point cloud 
+    pcl::PCLPointCloud2 input_cloud_pc2;
+    pcl_conversions::toPCL(*input_cloud,input_cloud_pc2);
+    pcl::fromPCLPointCloud2(input_cloud_pc2,*point_cloud);
+
+    // clip the point cloud in the desired X-Z plane
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud (point_cloud);
+    pass.setFilterFieldName ("x");
+    pass.setFilterLimits (-0.1, 0.1);
+    pass.filter (*plane_cloud);
+  }
+
+
+  // callback function for receiving the trajectory to check collision 
+  void trajectoryCallback(const geometry_msgs::PoseArrayConstPtr& trajectory)
+  {
+    if(plane_cloud->size()>1)
+    {
+      // create KD Tree 
+      pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;   
+      kdtree.setInputCloud (plane_cloud);
+
+      // set search parameters
+      float radius = 0.1; 
+      std_msgs::Bool result;
+      result.data = false;
+    
+      // search through th trajectory, checking for collisions
+      for(int i = 0; i < trajectory->poses.size(); i++)
+      {
+        // create search point
+        pcl::PointXYZ searchPoint;
+        searchPoint.x = trajectory->poses[i].position.x;
+        searchPoint.y = trajectory->poses[i].position.y;
+        searchPoint.z = trajectory->poses[i].position.z;
+
+        // setup search function
+        std::vector<int> pointIdxRadiusSearch;
+        std::vector<float> pointRadiusSquaredDistance;
+
+        // search 
+        if(kdtree.radiusSearch (searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
+  	    {
+	        // publish collision = true 
+	        result.data = true;
+    	    collision_pub.publish(result);
+	        break;
+  	    }
+      }
+  
+    // publish collision = false
+    if(result.data == false)
+      {
+	      collision_pub.publish(result);
+      }
+    }
+  }
+  
+};
+
+
+
+int main(int argc, char **argv)
+{
+  // init
+  ros::init(argc, argv, "collision_check");
+  
+  collision_checker cc; 
+
+  ros::spin();
+
+  return 0;
+}
+
+
